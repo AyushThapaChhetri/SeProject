1587d167de97b9a4b8c81f47c193ef47ee714cb2
abiralm <abiral.sayami@gmail.com>
2024-07-14 19:55:30 +0545

    added images and products.js was updated

--- src/products.js
+++ src/products.js
@@ -3,7 +3,7 @@
 export const Products =[
     {
         id:1,
-        productName:"Real Madrid Home",
+        productName:"Real Madrid 2023/24 Home",
         price:1200,
         category:'Home',
         productImage:"/real_home.jpg",
@@ -13,15 +13,15 @@ export const Products =[
     },
     {
         id:2,
-        productName:"Barca Classic",
+        productName:"Barcelona 1999 Home",
         price:1000,
-        category:'Away',
-        productImage:"barca_classic.jpg",
+        category:'Home',
+        productImage:"barca_classic.jpeg",
         favourite:false
     },
     {
         id:3,
-        productName:"Bayern Home",
+        productName:"Bayern Munich 2023/24 Home",
         price:1000,
         category:'Away',
         productImage:"bayern_home.jpg",
@@ -33,6 +33,142 @@ export const Products =[
         price:1500,
         category:'Home',
         productImage:"adarsh.jpg",
+        favourite:false
+    },
+    {
+        id:5,
+        productName:"AC Milan 2006/07 Home",
+        price:1200,
+        category:'Home',
+        productImage:"ac_milan.jpg",
+        favourite:true,
+        description:"lorem"
+
+    },
+    {
+        id:6,
+        productName:"Argentina 2020 Home",
+        price:1000,
+        category:'Home',
+        productImage:"arg.jpg",
+        favourite:false
+    },
+    {
+        id:7,
+        productName:"Bayern Home",
+        price:1000,
+        category:'Away',
+        productImage:"arsenal_retro.jpg",
+        favourite:false
+    },
+    {
+        id:8,
+        productName:"Arsenal 2023/24 Away",
+        price:1500,
+        category:'Away',
+        productImage:"arsenal.jpg",
+        favourite:false
+    },
+    {
+        id:9,
+        productName:"Brazil 2006 Home",
+        price:1200,
+        category:'Home',
+        productImage:"brazil.jpg",
+        favourite:true,
+        description:"lorem"
+
+    },
+    {
+        id:10,
+        productName:"Croatia 2018 Home",
+        price:1000,
+        category:'Home',
+        productImage:"croatia.jpg",
+        favourite:false
+    },
+    {
+        id:11,
+        productName:"England 2024 Away",
+        price:1000,
+        category:'Away',
+        productImage:"england.jpg",
+        favourite:false
+    },
+    {
+        id:12,
+        productName:"France 2006 Away",
+        price:1500,
+        category:'Away',
+        productImage:"france_retro.png",
+        favourite:false
+    },
+    {
+        id:13,
+        productName:"Germany 2014 Home",
+        price:1200,
+        category:'Home',
+        productImage:"ger.jpg",
+        favourite:true,
+        description:"lorem"
+
+    },
+    {
+        id:14,
+        productName:"Juventus 2014/15 Home",
+        price:1000,
+        category:'Home',
+        productImage:"juve.jpg",
+        favourite:false
+    },
+    {
+        id:15,
+        productName:"Liverpool 2004/05 Home",
+        price:1000,
+        category:'Home',
+        productImage:"liverpool.png",
+        favourite:false
+    },
+    {
+        id:16,
+        productName:"France 2024 Home",
+        price:1500,
+        category:'Home',
+        productImage:"france.png",
+        favourite:false
+    },
+    {
+        id:17,
+        productName:"Portugal 2024 Away",
+        price:1200,
+        category:'Away',
+        productImage:"portugal.png",
+        favourite:true,
+        description:"lorem"
+
+    },
+    {
+        id:18,
+        productName:"Real Madrid 2017/18 Away",
+        price:1000,
+        category:'Away',
+        productImage:"real_away.jpg",
+        favourite:false
+    },
+    {
+        id:19,
+        productName:"Spain 2010 Away",
+        price:1000,
+        category:'Away',
+        productImage:"spain.jpg",
+        favourite:false
+    },
+    {
+        id:20,
+        productName:"Manchester United 2007/08 Home",
+        price:1500,
+        category:'Home',
+        productImage:"united.jpg",
         favourite:false
     }
 ]
